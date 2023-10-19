76f1f1818d395b4a43fe8d0b682fc816a8a986a8
wenjia <zhangwj@bxlsj.com>
2023-10-19 16:46:34 +0800

    add terser plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/index.ts',
@@ -15,6 +16,7 @@ export default {
   plugins: [
     typescript({
       tsconfig: 'tsconfig.json'
-    })
+    }),
+    terser()
   ]
 }
